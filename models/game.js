2bfe89c856c47cafa31ceb63e5dfbdf4d0a02e56
SamSam Hashi <sammymadobe@gmail.com>
2025-07-25 12:24:08 -0500

    Finish creating API Resources schemas and models

---
+++ models/game.js
@@ -0,0 +1,64 @@
+const mongoose = require("mongoose");
+const validator = require("validator");
+
+const gameSchema = new mongoose.Schema({
+  name: {
+    type: String,
+    required: true,
+    minlength: 2,
+    maxlength: 80,
+  },
+
+  gameId: {
+    type: String,
+    required: [true, "The gameId is a required number"],
+    unique: true,
+  },
+
+  summary: {
+    type: String,
+    required: true,
+  },
+
+  releaseDate: {
+    type: String,
+    required: true,
+  },
+
+  genres: {
+    type: [String],
+    required: true,
+  },
+
+  platforms: {
+    type: [String],
+    required: true,
+  },
+
+  coverImage: {
+    type: String,
+    required: [true, "The coverImage is required"],
+    validate: {
+      validator(value) {
+        return validator.isURL(value);
+      },
+      message: "You have to enter a valid coverImage url",
+    },
+  },
+
+  rating: {
+    type: Number,
+    min: 0,
+    max: 100,
+    default: null,
+  },
+
+  owner: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: "User",
+    required: true,
+    select: false,
+  },
+});
+
+module.exports = mongoose.model("game", gameSchema);
