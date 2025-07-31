240fef3583bc47cf5214586be4529e588e24da9a
SamSam Hashi <sammymadobe@gmail.com>
2025-07-30 19:19:03 -0500

    Fix app crashing

--- routes/users.js
+++ routes/users.js
@@ -16,8 +16,8 @@ router.post("/signin", login); // NOT PROTECTED
 // auth middleware
 router.use(auth);
 
-router.post("/save"); // PROTECTED saving games to a user's collection
-router.get("/saved"); // PROTECTED retrieving a user's saved games
+router.post("/save", saveGames); // PROTECTED saving games to a user's collection
+router.get("/saved", savedGames); // PROTECTED retrieving a user's saved games
 router.get("/me", getCurrentUser); // PROTECTED getting current user info
 
 module.exports = router;
